ddacd7130c0b1a9ab346ed4663505cb05921d3df
kanghyein75 <kanghye675@gmail.com>
2026-05-14 19:28:36 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -20,6 +20,6 @@ int main() {
     printf("version: %u, size: %zu\n", s.version, sizeof(s.version));
     printf("ihl: %u, size: %zu\n", s.ihl, sizeof(s.ihl));
     printf("ttl: %llu, size: %zu\n", s.ttl, sizeof(s.ttl));
-    printf("protocl: %llu, size: %zu\n", s.protocol, sizeof(s.protocol));
+    printf("protocol: %llu, size: %zu\n", s.protocol, sizeof(s.protocol));
     printf("data: %s, size: %zu\n", s.data, sizeof(s.data));
 }
